40412a05228a9380e5adb2eaf16f31d37128f397
Bruno Faria <bnlf@hotmail.com>
2013-07-09 22:39:57 -0300

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
-var express = require('express');
+/*
+* @Author: Bruno Faria
+* Descript: Homework3 Part1 - Coursera Startup Engineering @ 2013
+* 			Sample Heroku Application	
+*/
 
+var express = require('express');
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var index = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
